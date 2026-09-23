e41510c19f7aa736c1133d412af01c5bc8bde0ae
Tuane TK <tuanykohlrausch@gmail.com>
2026-09-23 17:36:47 +0000

    deploy com ci/cd

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Add task
+        Add task com CI/CD
       </button>
       
       <Modal
